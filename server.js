d415e3bbd5d0dccf5e4350de9c55560ca3a228ce
Aayush7105 <aayushrawat5107@gmail.com>
2026-02-16 11:42:10 +0530

    initializing the project repo

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+const app = express();
+const port = 3000;
+import { readFile, writeFile } from "./modules/fileHandler.js";
+let employees = [];
+
+(async () => {
+  const data = await readFile("./employees.json", "utf-8");
+  employees = JSON.parse(data);
+})();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Employee Payroll System");
+});
+
+app.get("/employees", (req, res) => {
+  res.json(employees);
+});
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
